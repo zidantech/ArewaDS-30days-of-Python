eb0256bd76386f4a8312826f79a93f38cdfe1d20
zidantech <zidantech4@gmail.com>
2023-12-18 04:42:01 +0100

    jupyter ver. added, corrected

--- day_14/Higher_order_functions.py
+++ day_14/Higher_order_functions.py
@@ -1,34 +1,22 @@
 # Day 14: 30 days of Python programming
 
 # Exercises: Level 1
-countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
-names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Explain the difference between map, filter, and reduce.
-
-# Map ,filter and reduce bring some form of functional programming to python. 
-# All three functions take a function as the first argument and an iterable as the second argument of the function.
-# map and filter return an iterable, eg a list , though it actually returns a map object that can be cast into a list. 
-# map calls the function in the first argument and returns the result of the function for each of the elements in the iterable.
-# Filter on the other hand, only returns elements that satisfies the condition in the function argument. 
-# reduce is a bit different as it returns a single answer i.e. it reduces the iterable to a single answer.
-# It should be noted that map and filter functions return map objects and filter objects respectively.
-# Printing map and filter objects without casting to a list will give the memory location of the object.
+    # The difference between map, filter, and reduce in python is that they are three functions that allow you to apply a function to an iterable or a sequence of elements in a concise and elegant way.
+    # map(function, iterable) applies the function to each element in the iterable and returns a new iterable with the results. For example, map(str.upper, [“hello”, “world”]) returns [“HELLO”, “WORLD”].
+    # filter(function, iterable) keeps only the elements in the iterable that satisfy the function as a condition and returns a new iterable with the filtered elements. For example, filter(lambda x: x > 0, [-1, 2, -3, 4]) returns [2, 4].
+    # reduce(function, iterable) applies the function cumulatively to the elements in the iterable, from left to right, and reduces the iterable to a single value. For example, reduce(lambda x, y: x + y, [1, 2, 3, 4]) returns 10.
 
 # Explain the difference between higher order function, closure and decorator
-# A higher order function is a function that either takes in a function as a parameter or returns a function or both.
-# Decorators and closures are all higher order functions. 
-# Decorators return a closure and A closure is what is returned by a decorator.
-# Closures are used to preserve outer scope inside an inner scope,making variables act like they are private. 
-# Decorators are convenient ways to make changes to the functionality of code without making changes to the code. 
-# A decorator is written as a function closure
-# and implemented by giving the @ operator on top of the function.
+    # A higher order function is a function that takes another function as an argument or returns a function as a result. For example, the built-in function map is a higher order function that takes a function and an iterable and returns a new iterable with the function applied to each element.
+    # A closure is a function that can access and modify variables from the outer scope, even after the outer scope has ended. A closure is created when a nested function is returned by the outer function    
+    # A decorator is a special kind of higher order function that wraps another function and modifies its behavior or adds some functionality. A decorator can be defined using the @ syntax before the function definition
 
 # Define a call function before map, filter or reduce, see examples.
 # Example with map
-def square(x):
-    return x * x
+def square(y):
+    return y ** 3
 
 # List of numbers
 numbers = [1, 2, 3, 4, 5]
@@ -48,6 +36,7 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even_numbers = list(filter(is_even, numbers))
 print(even_numbers)
 
+
 # Use for loop to print each country in the countries list.
 for country in countries:
     print(country)
@@ -61,63 +50,6 @@ for number in numbers:
     print(number)
 
 # Exercise level 2
-# Use map to create a new list by changing each country to uppercase in the countries list
-upper_case_countries = list(map(lambda x:x.upper(),countries))
-print(f'countries in upper case: {upper_case_countries} ')
-# Use map to create a new list by changing each number to its square in the numbers list
-squared_numbers = list(map(lambda x:x**2,numbers))
-print(f'The square of the numbers: {squared_numbers} ')
-# Use map to change each name to uppercase in the names list
-upper_case_names = list(map(lambda x:x.upper(),names))
-print(f'Uppercase of the names: {upper_case_names} ')
-
-# Use filter to filter out countries containing 'land'.
-contains_land = list(filter(lambda x:'land' in x,countries))
-print(f' The countries with land in their name: {contains_land}')
-
-# Use filter to filter out countries having exactly six characters.
-six_chars = list(filter(lambda x:len(x)==6,countries))
-print(f'The countries with six characters: {six_chars} ')
-
-# Use filter to filter out countries containing six letters and more in the country list.
-six_or_more_chars = list(filter(lambda x:len(x) >= 6,countries))
-print(f'Countries with six or more characters in their names: {six_or_more_chars} ')
-
-
-# Use filter to filter out countries starting with an 'E'
-starting_with_e = list(filter(lambda x:x.startswith('E'),countries))
-print(f' Countries starting with E in the list are: {starting_with_e}')
-
-# Chain two or more list iterators (eg. arr.map(callback).filter(callback).reduce(callback))
-from functools import reduce
-
-numbers = [3, 5, 8, 2, 14]
-squared_even_numbers_sum = reduce(lambda x, y: x + y, 
-                                   list(filter(lambda x: x % 2 == 0, 
-                                                map(lambda x: x**2, numbers))))
-print(squared_even_numbers_sum)
-
-# Declare a function called get_string_lists which takes a list as a parameter and then returns a list containing only string items.
-def get_string_lists(input_list):
-    return [item for item in input_list if isinstance(item, str)]
-
-# Test the function
-input_list = [1, 'apple', 'banana', 3, 'orange', 'kiwi']
-result = get_string_lists(input_list)
-print(result)
-
-# Use reduce to sum all the numbers in the numbers list.
-from functools import reduce
-def add(x,y):
-    return x + y
-sum_all = reduce(add,numbers)
-print(f'The sum of {numbers} is {sum_all}')
-
-# Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and Iceland are north European countries
-concat = reduce(lambda x,y:x + ',' + ' '+ y,countries[:-1])
-print(f'{concat} and {countries[-1]} are North European countries ')
-
-# Declare a function called categorize_countries that returns a list of countries with some common pattern (you can find the countries list in this repository as countries.js(eg 'land', 'ia', 'island', 'stan')).
 # countries list
 countries = [
   'Afghanistan',
@@ -314,8 +246,65 @@ countries = [
   'Zambia',
   'Zimbabwe',
 ]
-# Declare a function called categorize_countries that returns a list of countries with some common pattern 
-# (you can find the countries list in this repository as countries.js(eg 'land', 'ia', 'island', 'stan')).
+
+# Use map to create a new list by changing each country to uppercase in the countries list
+upper_case_countries = list(map(lambda x:x.upper(),countries))
+print(f'countries in upper case: {upper_case_countries} ')
+
+# Use map to create a new list by changing each number to its square in the numbers list
+squared_numbers = list(map(lambda x:x**2,numbers))
+print(f'The square of the numbers: {squared_numbers} ')
+
+# Use map to change each name to uppercase in the names list
+upper_case_names = list(map(lambda x:x.upper(),names))
+print(f'Uppercase of the names: {upper_case_names} ')
+
+# Use filter to filter out countries containing 'land'.
+contains_land = list(filter(lambda x:'land' in x,countries))
+print(f' The countries with land in their name: {contains_land}')
+
+# Use filter to filter out countries having exactly six characters.
+six_chars = list(filter(lambda x:len(x)==6,countries))
+print(f'The countries with six characters: {six_chars} ')
+
+# Use filter to filter out countries containing six letters and more in the country list.
+six_or_more_chars = list(filter(lambda x:len(x) >= 6,countries))
+print(f'Countries with six or more characters in their names: {six_or_more_chars} ')
+
+# Use filter to filter out countries starting with an 'E'
+starting_with_e = list(filter(lambda x:x.startswith('E'),countries))
+print(f' Countries starting with E in the list are: {starting_with_e}')
+
+# Chain two or more list iterators (eg. arr.map(callback).filter(callback).reduce(callback))
+from functools import reduce
+
+numbers = [3, 5, 8, 2, 14]
+squared_even_numbers_sum = reduce(lambda x, y: x + y, 
+                                   list(filter(lambda x: x % 2 == 0, 
+                                                map(lambda x: x**2, numbers))))
+print(squared_even_numbers_sum)
+
+# Declare a function called get_string_lists which takes a list as a parameter and then returns a list containing only string items.
+def get_string_lists(input_list):
+    return [item for item in input_list if isinstance(item, str)]
+
+# Test the function
+input_list = [1, 'apple', 'banana', 3, 'orange', 'kiwi']
+result = get_string_lists(input_list)
+print(result)
+
+# Use reduce to sum all the numbers in the numbers list.
+from functools import reduce
+def add(x,y):
+    return x + y
+sum_all = reduce(add,numbers)
+print(f'The sum of {numbers} is {sum_all}')
+
+# Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and Iceland are north European countries
+concat = reduce(lambda x,y:x + ',' + ' '+ y,countries[:-1])
+print(f'{concat} and {countries[-1]} are North European countries ')
+
+# Declare a function called categorize_countries that returns a list of countries with some common pattern (you can find the countries list in this repository as countries.js(eg 'land', 'ia', 'island', 'stan')).
 def categorize_countries(pattern):
     list_countries = []
     for country in countries:
